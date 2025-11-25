cdef6e4e3fb35ef163ef9adf9754b48670834992
ridhoni09 <ridhoni09@gmail.com>
2025-11-25 16:10:49 +0800

    BAB 1 & 2

---
+++ hallo.js
@@ -0,0 +1,16 @@
+        // Variable Tipe Data
+        var nama ="Alice"
+        var umur = 25;
+        var aktif = true;
+
+        // Operator
+        var x = 10;
+        var y = 5;
+        var hasil = x + y;
+
+        // Kondisi
+        if (umur > 18) {
+            alert("Hai " + nama +",  usia Anda sudah dewasa");
+        } else {
+            alert("Hai " + nama +",   usia Anda masih di bawah umur");
+        }
---
+++ sapa.js
@@ -0,0 +1,4 @@
+var nama = document.getElementById("inputNama").value;
+function sapa() {
+    alert("Halo "+ nama +", Selamat Datang di Profil Saya!");
+}
